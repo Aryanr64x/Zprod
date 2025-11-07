1b60d9ee4eb5f256f77d9a24578b629db8318bca
saket.aryan <saket.aryanr64x@gmail.com>
2025-11-07 21:44:41 +0530

    created base app

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { Link } from "expo-router"
+import { Text, View } from "react-native"
+
+const Home = ()=>{
+    return <View>
+        <Text> Hey everyone! This is app created by Mayukh Banerjee and Aryan 
+            Saket. So lets get fucking STARTED!!!!! </Text>
+        
+    </View>
+}
+
+export default Home
